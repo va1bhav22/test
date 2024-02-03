41e54bce8975ee90d7acd11ee4f5deb71ff976a4
vaibhav21898 <vj2598054@gmail.com>
2024-02-03 22:49:45 +0530

    some changes

--- src/pages/postingSerch.jsx
+++ src/pages/postingSerch.jsx
@@ -1,39 +1,40 @@
 // components/Posts.js
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllPosts, searchPosts } from "../redux/actions/postActions";
 
-const SearchPosts = () => {
+const Posts = () => {
   const dispatch = useDispatch();
   const { posts, loading, error } = useSelector((state) => state.post);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     dispatch(getAllPosts());
   }, [dispatch]);
 
-  const handleSearch = (query) => {
-    dispatch(searchPosts(query));
+  const handleSearch = () => {
+    dispatch(searchPosts(searchQuery));
   };
-  console.log(posts);
+
   return (
     <div>
       <h2>Posts</h2>
       {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
+
       <input
         type="text"
         placeholder="Search"
-        onChange={(e) => handleSearch(e.target.value)}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
       />
+      <button onClick={handleSearch}>Search</button>
       <ul>
         {posts?.data?.posts?.map((post) => (
-          <li key={post.id} className="text-red-800">
-            {post.caption}
-          </li>
+          <li key={post.id}>{post.caption}</li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default SearchPosts;
+export default Posts;
